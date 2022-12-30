5d90d4f9ae0c9f259e9d0022a41caca7fb69057b
Jeep2333 <55258293+Jeep2333@users.noreply.github.com>
2022-12-30 03:47:52 -0500

    demo

---
+++ demo-nosecurity/src/main/java/com/example/demonosecurity/DemoNosecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demonosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNosecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoNosecurityApplication.class, args);
+    }
+
+}
---
+++ demo-nosecurity/src/test/java/com/example/demonosecurity/DemoNosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demonosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNosecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
