da749ddfe5c053b0d6a946c311b8602319a46435
Andy Dai <jdai023@uottawa.ca>
2022-12-31 01:59:03 -0500

    Api for encode and decode the uml

--- nosecurity/src/main/java/com/example/nosecurity/controller/LoginController.java
+++ nosecurity/src/main/java/com/example/nosecurity/controller/LoginController.java
@@ -3,14 +3,18 @@ package com.example.nosecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 public class LoginController {
+    Map<Integer, String> map = new HashMap<>();
     @RequestMapping("/user-login")
     public String index(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
         if(!"user".equals(username)){
@@ -33,6 +37,20 @@ public class LoginController {
     public String login() {
         return "login";
     }
-    // Login form with error
+    @RequestMapping("/encode")
+    @ResponseBody
+
+    public String encode(String longUrl) {
+        map.put(longUrl.hashCode(), longUrl);
+        return "http://tinyurl.com/" + longUrl.hashCode();
+
+    }
+    @RequestMapping("/decode")
+    @ResponseBody
+    public String decode(String shortUrl) {
+        return (map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", ""))));
+    }
+
+
 
 }
